1a80389b476ae9eeb82b26f818507d409c63a7e1
Jardel Kellemann <125585600+jardelkll@users.noreply.github.com>
2023-07-19 20:09:17 -0300

    Estudos de Vue | mini projeto como aplicação de funcionalidades para fins didádicos

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Banner/>
+    <div class="main-container">
+      <h1>Monte o seu burger:</h1>
+      <p></p>
+      <Form/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue"
+import Form from "../components/Form.vue"
+
+export default {
+  name: 'HomeView',
+  components: {
+    Banner,
+    Form
+  }
+}
+</script>
